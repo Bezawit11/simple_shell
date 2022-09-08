fa52cbd67bfe69f608acccb4c2d0dc9605623b2d
mystic5phoenix <106801524+mystic5phoenix@users.noreply.github.com>
2022-09-08 09:59:28 -0700

    Update play_on_strings.c

--- play_on_strings.c
+++ play_on_strings.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- *
- *
+ *_strlen - finds the length of a string
+ *@s: string input
+ *Return: returns length
  */
 int _strlen(char *s)
 
@@ -17,8 +18,10 @@ return (l);
 }
 
 /**
- *
- *
+ *_strcmp - compares two strings
+ *@s1: string input
+ *@s2: string input
+ *Return: returns 0 if matched
  */
 int _strcmp(char *s1, char *s2)
 
