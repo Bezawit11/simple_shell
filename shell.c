664e80c38c6b2eee65ee206e35f91c3d3bb4002a
Bezawit11 <socbeza13@gmail.com>
2022-09-08 06:05:11 -0700

    editied execute

--- shell.c
+++ shell.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/types.h>
 #include <stdlib.h>
 #include "main.h"
 
@@ -9,14 +12,25 @@
 int main(void)
 
 {
-int h = 1;
+int h = 1, status;
 char *buffer;
 char **tokenz;
+pid_t pid;
 while (h == 1)
 {
 buffer = readcmd();
 tokenz = parse_line(buffer);
-
+pid = fork();
+if (pid == 0)
+{
+if (execve(tokenz[0], tokenz, environ) == -1)
+perror("Error");
+exit(0);
+}
+else if (pid < 0)
+perror("Error");
+else
+wait(&status);
 free(buffer);
 free(tokenz);
 }
