f1b9ffaf55e8102bc8d06bac1b8f24079bc75acc
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-09-12 01:51:43 -0700

    Update parse_line.c

--- parse_line.c
+++ parse_line.c
@@ -8,23 +8,23 @@
  *@buffer: command to be tokenized
  *Return: returns a double poiner of tokenized command
  */
-char **parse_line(char *buffer)
+char **parse_line(char *buffer, char **tokenz)
 
 {
 int i = 0, size = 64;
 char *tok;
-char **tokenp = malloc(size * sizeof(char *));
-if (tokenp == NULL)
+tokenz = malloc(size * sizeof(char *));
+if (tokenz == NULL)
 {
 exit(EXIT_FAILURE);
 }
 tok = strtok(buffer, " ");
 while (tok != NULL)
 {
-tokenp[i] = tok;
+tokenz[i] = tok;
 tok = strtok(NULL, " ");
 i = i + 1;
 }
-tokenp[i] = NULL;
-return (tokenp);
+tokenz[i] = NULL;
+return (tokenz);
 }
