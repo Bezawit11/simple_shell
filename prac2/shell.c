d001961e4436c3896d1052d7ed6516d1ee062a2a
Bezawit11 <socbeza13@gmail.com>
2022-09-25 07:34:31 -0700

    new big

--- prac2/shell.c
+++ prac2/shell.c
@@ -19,7 +19,7 @@ while (h == 1)
 {
 p = isatty(STDIN_FILENO);
 buffer = readcmd(buffer);
-tokenz = parse_line(buffer, tokenz, m);
+tokenz = parse_line(buffer, tokenz);
 c = check_builtin(tokenz);
 if (c == 0){
 free(buffer);
